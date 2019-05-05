3d65d67ccc48742e27d727eaa69fe5101985db36
Samuel Afolaranmi <sammysgame.dev@gmail.com>
2019-05-05 20:06:28 +0100

    chore - setup env & base file structure

---
+++ twitter_credentials.py
@@ -0,0 +1,7 @@
+from os import getenv
+
+# Variables to access Twitter API
+ACCESS_TOKEN = getenv('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = getenv('ACCESS_TOKEN_SECRET')
+CONSUMER_KEY = getenv('CONSUMER_KEY')
+CONSUMER_SECRET = getenv('CONSUMER_SECRET')
